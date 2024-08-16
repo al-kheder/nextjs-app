863311df8c02b0a2366bcc629277338ab01344fd
AnasRezk <anasrizk01@gmail.com>
2024-08-16 18:10:13 +0200

    Fix Slider

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,76 +1,12 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
-import { ModeToggle } from '@/components/ModeToggle';
 import Header from '@/components/Header';
-import { useTranslations } from 'next-intl';
-import LocalSwitscher from '@/components/LocalSwitscher';
-import Category from '@/components/FoodCategorySlider';
 import Footer from '@/components/Footer';
-import FoodCategorySlider from '@/components/FoodCategorySlider';
 import Slider from '@/components/Slider';
 import OrderCard from '@/components/OrderCard';
 
 export default function Home() {
-  const t = useTranslations('HomePage');
-
   return (
     <section className=" px-40 py-20 w-full flex flex-col justify-center">
       <div className="flex flex-col  justify-center   w-full ">
-        {/*   <div className="flex justify-between">
-          <LocalSwitscher/>
-          <h1 className="text-3xl font-bold">{t('pageTitle')}</h1>
-          <ModeToggle />
-        </div>
-        <Card className="w-[350px] mt-10">
-          <CardHeader>
-            <CardTitle>{t('cardTitle')}</CardTitle>
-            <CardDescription>{t('cardDescription')}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <form>
-              <div className="grid w-full items-center gap-4">
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="name">{t('name')}</Label>
-                  <Input id="name" placeholder="Name of your project" />
-                </div>
-                <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Framework</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">Next.js</SelectItem>
-                      <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                      <SelectItem value="astro">Astro</SelectItem>
-                      <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
-            </form>
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            <Button variant="outline">Cancel</Button>
-            <Button>Deploy</Button>
-          </CardFooter>
-        </Card> */}
         <Header />
         <div>
           <Slider />
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,7 +1,3 @@
-'use client';
-import { ChevronRight, ChevronLeft } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { useState } from 'react';
 import {
   Carousel,
   CarouselContent,
@@ -10,8 +6,6 @@ import {
   CarouselPrevious,
 } from '@/components/ui/carousel';
 import { Card, CardContent } from '@/components/ui/card';
-
-//import { useTranslations } from 'next-intl';
 
 export default function Slider() {
   const categories = [
@@ -22,9 +16,9 @@ export default function Slider() {
     'Salads',
     'Desserts',
   ];
-  /*
+
   return (
-     <Carousel
+    <Carousel
       opts={{
         align: 'center',
         loop: true,
@@ -47,58 +41,5 @@ export default function Slider() {
       <CarouselPrevious />
       <CarouselNext />
     </Carousel>
-  ); */
-
-  /*  const categories = [
-    t('categories[0]', { default: 'Pizza' }),
-    t('categories[1]', { default: 'Burgers' }),
-    t('categories[2]', { default: 'Sushi' }),
-    t('categories[3]', { default: 'Pasta' }),
-    t('categories[4]', { default: 'Salads' }),
-    t('categories[5]', { default: 'Desserts' }),
-  ]; */
-  
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const visibleCategory = categories.slice(currentIndex, currentIndex + 3);
-  const handlePrev = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex(currentIndex - 1);
-    }
-  };
-
-  const handleNext = () => {
-    if (currentIndex < categories.length - 3) {
-      setCurrentIndex(currentIndex + 1);
-    }
-  };
-  return (
-    <div className="p-4 flex justify-between  items-center">
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={handlePrev}
-        disabled={currentIndex === 0}
-      >
-        <ChevronRight className="h-4 w-4" />
-      </Button>
-      <div className="flex justify-between ">
-        {visibleCategory.map((category, index) => {
-          return (
-            <div key={index} className="flex p-1">
-              <div className="p-3 "> {category}</div>
-            </div>
-          );
-        })}
-      </div>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={handleNext}
-        disabled={currentIndex >= categories.length - 3}
-      >
-        <ChevronLeft className="h-4 w-4" />
-      </Button>
-    </div>
   );
-  
 }
